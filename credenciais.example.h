cb59a3de503dee1b1fc4c696b864169c9856c175
Mateus Fernandes Santos <ferrmathh2@gmail.com>
2025-06-04 22:48:29 -0300

    feat: arquivo exemplo de credenciais

---
+++ credenciais.example.h
@@ -0,0 +1,5 @@
+#define WIFI_SSID ""                    // Substitua pelo nome da sua rede Wi-Fi
+#define WIFI_PASSWORD ""                // Substitua pela senha da sua rede Wi-Fi
+#define MQTT_SERVER ""                  // Substitua pelo endereço do host - broket MQTT: Ex: 192.168.1.107
+#define MQTT_USERNAME ""                // Substitua pelo nome da host MQTT - Username
+#define MQTT_PASSWORD ""                // Substitua pelo Password da host MQTT - credencial de acesso - caso exista
